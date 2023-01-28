342888846c2e70fa9a8372272626a3e36f06de87
rene-marcker <rene-marcker@hotmail.com>
2023-01-28 17:18:41 +0100

    fix: the naming

--- src/components/Translations/TranslationOutput.jsx
+++ src/components/Translations/TranslationOutput.jsx
@@ -1,9 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Col, Container, Row } from "react-bootstrap";
 import "./Translation.css";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-
 
 const TranslationOutput = (props) => {
 
